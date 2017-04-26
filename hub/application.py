1bde1332a24035e4f121ee7c17640e7f0c4aa7aa
How2Compute <howtocompute123@gmail.com>
2017-04-26 11:11:26 +0000

    Added notification and registration functionality (but no GUI)

--- hub/application.py
+++ hub/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, session, abort
+from flask import Flask, jsonify, render_template, session, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from helpers import *
 
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
+# Model to store information about devices
 class Device(db.Model):
     __tablename__ = 'clients'
     
@@ -16,12 +17,23 @@ class Device(db.Model):
     name = db.Column(db.Text)
     api_key = db.Column(db.Text)
     active = db.Column(db.Boolean)
-    permission_level = db.Column(db.Integer)
+    access_level = db.Column(db.Integer)
     
-    def __init__(name, permission_level):
+    def __init__(self, name, permission_level):
         self.name = name
-        self.permission_level = permission_level
+        self.access_level = permission_level
         self.api_key = generate_api_token()
+
+# Model to store notifications   
+class Notification(db.Model):
+    __tablename__ = 'notifications'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String)
+    dismissed = db.Column(db.Boolean)
+    
+    def __init__(self, body):
+        self.body = body
 
 # GUI
 @app.route('/')
@@ -61,18 +73,28 @@ def api_ui():
 #   -- Register Device
 @app.route('/api/0.1/devices', methods=['POST'])
 def register():
+    # Was json entered?
+    if not request.json:
+        abort(400)
     # Was the name provided?
-    if 'name' not in request.json or type(request.json.get('name') != string):
+    elif 'name' not in request.json:# or type(request.json.get('name')) != string:
         abort(400)
     # Was the permission level provided?    
-    elif 'permission_level' not in request.json or type(request.json.get('permission_level') != integer):
+    elif 'permission_level' not in request.json:# or type(request.json.get('permission_level') != integer):
         abort(400)
     
     # Insert into devices table as pending
-    device = Device()
-    # Notify User
+    device = Device(request.json.get('name'), request.json.get('permission_level'))
+    db.session.add(device)
+    db.session.commit()
+    # Notify User TODO make this also hold a title, call to action URL/URI etc
+    notification = Notification("A new device just tried to register! Please approve it in your devices page.")
+    db.session.add(notification)
+    db.session.commit();
+    
     # Return successfully made request (but not yet approved). Also send API keys etc
     return jsonify({
         "status": "Unapproved",
-        "credentials": #TODO
+        "id": device.id,
+        "api_key": device.api_key
     })
