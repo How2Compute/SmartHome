13b5cdccae96b18af2ee08d70ade670e768fdb74
How2Compute <howtocompute123@gmail.com>
2017-04-27 18:30:05 +0000

    Cleaned client and factored out update into function.

--- cli/client.py
+++ cli/client.py
@@ -1,19 +1,17 @@
 from library import SmartHomeApi
-
+import time
 # Create the client with pre-existing credentials
 api = SmartHomeApi("http://localhost:5000/api/0.1", id=1, api_key="api_LgyPqdKqxr8hiHcH5EHwBiNg62QsFJk4aYqb32YPIfhgQJ")
-#api.RegisterDevice("hey", 0)
-print("Can't test register device!")
-print("Testing GetDevices...")
-print(api.GetDevices())
-print("Testing GetDeviceStatus for self...")
-print(api.GetDeviceStatus())
-print("Testing GetDeviceStatus for #2...")
-print(api.GetDeviceStatus(device_id=2))
-print("Testing SetNewStatus for self...")
-api.SetNewStatus("TODO make this random") # doesn't work!
-print("passed!" if api.GetDeviceStatus() == "TODO make this random" else "failed")
-print("Testing SetNewStatus for self...")
-api.SetNewStatus("TODO make this random", device_id=2) # doesn't work!
-print("passed!" if api.GetDeviceStatus(device_id=2) == "TODO make this random" else "failed")
-print("Can't test delete device!")
+
+last_status = "UNKNOWN"
+def doUpdate(status):
+    # TODO
+    print("updating! (stauts: {})".format(status))
+    
+while True:
+    status = api.GetDeviceStatus()
+    if last_status != status:
+        # Changed!
+        doUpdate(status)
+        last_status = status
+    time.sleep(1)
