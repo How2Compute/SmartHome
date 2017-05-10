feed9a632cba3d733f3b09df9a4203aaf49f1473
How2Compute <howtocompute123@gmail.com>
2017-05-10 12:47:06 +0200

    Update demo0.py
    Improved commenting on demo0.

--- cli/demo0.py
+++ cli/demo0.py
@@ -4,28 +4,38 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 
-# 7  -> LED
+# Board Pin 7  -> LED
 
-# Create the client with pre-existing credentials
+# Connect to the API on localhost, port 5000 with the credentials of id->10 and api_key->api_Lg...QJ
 api = SmartHomeApi("http://localhost:5000/api/0.1", id=10, api_key="api_LgyPqdKqxr8hiHcH5EHwBiNg62QsFJk4aYqb32YPIfhgQJ")
 
+# Set the raspberry to use BOARD mode (how to handle pin numbers)
 GPIO.setmode(GPIO.BOARD)
+# Set GPIO pin 7 as an output
 GPIO.setup(7, GPIO.OUT)
 
+# Set the last status to UNKNOWN (so that it should update in the later loop)
 last_status = "UNKNOWN"
 
+# Function that handles updating the light
 def doUpdate(status):
     if status == "ON":
+        # Switch the LED on.
         GPIO.output(7, GPIO.HIGH)
     else:
+        # Switch the LED off.
         GPIO.output(7, GPIO.LOW)
     
     print("updating! (status: {})".format(status))
 
+# Main Loop
 while True:
+    # Query the API (and in turn the HUB) for the apps' status
     status = api.GetDeviceStatus()
+    # Check if the status has changed
     if last_status != status:
-        # Changed!
+        # If it has, call the update function passing in the new status
         doUpdate(status)
         last_status = status
+    # Wait for 1 second in order to not stress the API
     time.sleep(1)
