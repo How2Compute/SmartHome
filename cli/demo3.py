459332c438af874222b951d3b3ce3aa5a4d534a7
How2Compute <howtocompute123@gmail.com>
2017-05-10 12:41:33 +0200

    Update demo3.py
    Improved commenting on demo3

--- cli/demo3.py
+++ cli/demo3.py
@@ -1,6 +1,7 @@
+# Import the SmartHome! library
 from library import SmartHomeApi
 
-
-# Create and register the client
+# Connect to the API, running on localhost, port 5000
 api = SmartHomeApi("http://localhost:5000/api/0.1")
+# Register a device with the name: "CoolName" and permission level 5 
 api.RegisterDevice("CoolName", 5)
